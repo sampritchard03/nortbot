14f5082bf647c1144e2b37a48afef18d985ca695
sampritchard428 <sampritchard428@gmail.com>
2025-11-08 11:44:24 -0500

    something

--- index.js
+++ index.js
@@ -6,12 +6,27 @@ import {loader as autoEat} from "mineflayer-auto-eat"
 import armorManager from "mineflayer-armor-manager"
 import {plugin as pvp} from "mineflayer-pvp"
 
+function ai(bot) { // extremely simple ai logic
+    if (bot.enemy) {
+        if (bot.time % 5 == 0) bot.pvp.attack(bot.enemy)
+    } else {
+        bot.pvp.stop()
+        if (bot.groundItem) {
+            if (bot.time % 5 == 0) bot.pathfinder.setGoal(new goals.GoalFollow(bot.groundItem.entity, 0))
+        } else {
+            bot.emit("idleTick")
+        }
+        
+    }
+}
+
+
 export function plugin(bot) {
 
     bot.ownerName = "Slaals" // default to me >:) you have to change it manually
     bot.enemyRange = 24
     bot.groundItemRange = 6
-    bot.aiTick = () => {}
+    bot.time = 0
 
     // getters for nearest enemy, nearest ground item, and the owner entity.
     Object.defineProperty(bot, 'enemy', {
@@ -52,24 +67,16 @@ export function plugin(bot) {
             bot.autoEat.enableAuto()
 
             // more initialization stuff
+
+            bot.on("physicsTick", () => {
+                ai(bot)
+                bot.time++
+            })
+
+            bot.emit("init")
         })
     })
 
-    var time = 0
-    bot.on("physicTick", () => { // extremely simple ai logic
-        if (bot.enemy) {
-            if (time % 5 == 0) bot.pvp.attack(bot.enemy)
-        } else {
-            bot.pvp.stop()
-            if (bot.groundItem) {
-                if (time % 5 == 0) bot.pathfinder.setGoal(new goals.GoalFollow(bot.groundItem.entity, 0))
-            } else {
-                bot.aiTick()
-            }
-            
-        }
-
-        time++
-    })
+    
 }
 
